5b612ed2c2cbb07d6741701ce51e3ca77b0f3ea0
bobbiaditya <bobbi.aditya@gmail.com>
2019-11-13 17:31:58 +0700

    enable

--- static/js/auth.js
+++ static/js/auth.js
@@ -31,30 +31,29 @@ function hasQueryString() {
 // Handle the user's login and what happens next.
 function handleLogin() {
   // If the user is logging in for the first time...
-  if (okta.token.hasTokensInUrl()) {
-    okta.token.parseTokensFromUrl(
-      function success(res) {
-        // Save the tokens for later use, e.g. if the page gets refreshed:
-        okta.tokenManager.add("accessToken", res[0]);
-        okta.tokenManager.add("idToken", res[1]);
+  // if (okta.token.hasTokensInUrl()) {
+  //   okta.token.parseTokensFromUrl(
+  //     function success(res) {
+  //       // Save the tokens for later use, e.g. if the page gets refreshed:
+  //       okta.tokenManager.add("accessToken", res[0]);
+  //       okta.tokenManager.add("idToken", res[1]);
 
-        // Redirect to this user's dedicated room URL.
-        window.location = getRoomURL();
-      }, function error(err) {
-        alert("We weren't able to log you in, something horrible must have happened. Please refresh the page.");
-      }
-    );
-  } else {
-    okta.session.get(function(res) {
+  //       // Redirect to this user's dedicated room URL.
+  //       window.location = getRoomURL();
+  //     }, function error(err) {
+  //       alert("We weren't able to log you in, something horrible must have happened. Please refresh the page.");
+  //     }
+  //   );
+  // } else {
+  //   okta.session.get(function(res) {
 
-      // If the user is logged in, display the app.
-      if (res.status === "ACTIVE") {
+  //     // If the user is logged in, display the app.
+  //     if (res.status === "ACTIVE") {
 
-        // If the user is logged in on the home page, redirect to their room page.
-        if (!hasQueryString()) {
-          window.location = getRoomURL();
-        }
-
+  //       // If the user is logged in on the home page, redirect to their room page.
+  //       if (!hasQueryString()) {
+  //         window.location = getRoomURL();
+  //       }
         return enableVideo();
       }
 
